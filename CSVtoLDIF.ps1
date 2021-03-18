65d6672eb5836844006128f8475e783bb9ca91e6
Callum Stringer <callumstringer10@gmail.com>
2021-03-18 14:12:21 +0100

    :sparkles: init

---
+++ CSVtoLDIF.ps1
@@ -0,0 +1,44 @@
+﻿# Dieses Powershell Skript importiert Einträge aus einer CSV Datei und erstellt daraus ein LDIF File
+# Autor: David Schaad
+
+#$csvFile = read-host "Bitte geben Sie die zu importierende Datei(.csv) inklusive Pfad an"
+$csvFile = "C:\SCDA-Workspace\Desktop\Adressliste.csv"
+
+#Einlesen CSV Datei
+$importCSV = import-csv -Path $csvFile -Encoding Default -Delimiter ';'
+
+#Speicherpfad und Dateiname für die LDIF Datei
+$ldifFile = "C:\SCDA-Workspace\Desktop\Adressliste.ldif"
+
+#Optionales Zählen der Datensätze
+$Counter = 1
+
+#Durchlaufen der Datensaetze der CSV
+$object = ForEach ($user in $importCSV)
+    {   
+#Auslesen und Zuweisen der Attribute aus der CSV in eigene Variablen                
+        $Name = $user.Name	
+        $Vorname = $user.Vorname	
+        $Adresse = $user.Adresse	
+        $Hausnummer = $user.Hausnummer	
+        $Land = $user.Land	
+        $PLZ = $user.PLZ
+        $Ort = $user.Ort
+
+#Addieren der Strings
+"#Entry Number: " + $Counter++
+"dn: cn=" + $Vorname + " " + $Name + ",ou=kontakte,dc=schaad,dc=lan"
+"cn: " + $Vorname + " " + $Name
+"givenname: " + $Vorname
+"l: " + $Ort
+"objectClass: inetOrgPerson"
+"objectClass: top"
+"postalcode: " + $PLZ
+"sn: " + $Name
+"st: " + $Land
+"street: " + $Adresse + " " + $Hausnummer
+""
+
+$object | Add-Content $ldifFile
+
+}
---
+++ setup-docker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+#sudo pacman -Rcns docker
+#sudo pacman -S docker
+#sudo systemctl start docker
+
+sudo docker stop ldap-service
+sudo docker rm ldap-service
+
+sudo docker stop phpldapadmin-service
+sudo docker rm phpldapadmin-service
+
+sudo docker stop owncloud
+sudo docker rm owncloud
+
+sudo docker run \
+    --name ldap-service \
+    --hostname ldap-service \
+	--env LDAP_ORGANISATION="gerzenstein" \
+	--env LDAP_DOMAIN="gerzenstein.com" \
+	--env LDAP_ADMIN_PASSWORD="admin" \
+	--detach osixia/openldap:1.1.8
+
+sudo docker run --name phpldapadmin-service -p 9090:80 --hostname phpldapadmin-service --link ldap-service:ldap-host --env PHPLDAPADMIN_HTTPS=false --env PHPLDAPADMIN_LDAP_HOSTS=ldap-host --detach osixia/phpldapadmin:0.9.0
+
+sudo docker run --name  owncloud -d -p 8080:80 owncloud:8.1
+
+PHPLDAP_IP=$(sudo docker inspect -f "{{ .NetworkSettings.IPAddress }}" phpldapadmin-service)
+
+echo "Go to: https://$PHPLDAP_IP"
+echo "Login DN: cn=admin,dc=geryenstein,dc=com"
+echo "Password: admin"
