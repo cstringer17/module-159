baf66bb2fe4b0a90e4c7453b44d6e4dc626c4a8b
cstringer17 <callumstringer10@gmail.com>
2021-03-21 13:49:52 +0100

    volume fix maybe

--- setup-docker.sh
+++ setup-docker.sh
@@ -20,7 +20,7 @@ sudo docker run \
 	--env LDAP_DOMAIN="gerzenstein.com" \
 	--env LDAP_ADMIN_PASSWORD="admin" \
 	-v /data/openldap/database:/var/lib/ldap \
-	-v /data/openldap/config:/etc/ldap \
+	-v /data/openldap/config:/etc/ldap/slap.d \
 	--detach osixia/openldap:1.1.8
 
 sudo docker run --name phpldapadmin-service -p 9090:80 --hostname phpldapadmin-service --link ldap-service:ldap-host --env PHPLDAPADMIN_HTTPS=false --env PHPLDAPADMIN_LDAP_HOSTS=ldap-host --detach osixia/phpldapadmin:0.9.0
